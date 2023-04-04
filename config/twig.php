b7d266b87d220d76abdf08d1712dfbb6259b1c3b
CoHillDev <[colinemv.dev@gmail.com](mailto:colinemv.dev@gmail.com)>
2023-04-05 00:55:25 +0200

    Rendu Twig Quest 3

---
+++ config/twig.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new Twig\Environment($loader, ['debug' => true]);
+$twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale mail à l'initialisation de l'objet Twig
+$contactEmail = 'none.of@your.biz';
+$twig->addGlobal('contactEmail', $contactEmail);
